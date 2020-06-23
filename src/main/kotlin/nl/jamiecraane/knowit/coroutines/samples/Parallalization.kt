04de4fc2dd9d84e53e8a8e82090b796f4e33f4ab
jamiecraane <jcraane@gmail.com>
2020-06-23 13:49:42 +0200

    Add parallel example

---
+++ src/main/kotlin/nl/jamiecraane/knowit/coroutines/samples/Parallalization.kt
@@ -0,0 +1,49 @@
+package nl.jamiecraane.knowit.coroutines.samples
+
+import kotlinx.coroutines.*
+import nl.jamiecraane.knowit.shared.log
+import kotlin.system.measureTimeMillis
+
+fun main() = runBlocking<Unit> {
+    sequential()
+//    parallel()
+
+}
+
+private fun CoroutineScope.parallel() {
+    println("Parallel")
+    launch {
+        val time = measureTimeMillis {
+            val results = mutableListOf<Int>()
+            val jobs = mutableListOf<Job>()
+            for (i in 0..100) {
+                jobs += launch {
+                    results += expensiveOperation(i)
+                }
+            }
+            jobs.joinAll()
+            println(results)
+        }
+        println("time: $time")
+    }
+}
+
+private fun CoroutineScope.sequential() {
+    println("Sequential")
+    launch {
+        val time = measureTimeMillis {
+            val results = mutableListOf<Int>()
+            for (i in 0..100) {
+                results += expensiveOperation(i)
+            }
+            println(results)
+        }
+        println("time: $time")
+    }
+}
+
+private suspend fun expensiveOperation(input: Int): Int {
+    log("Computing $input")
+    delay(100)
+    return input * 10
+}
---
+++ src/main/kotlin/nl/jamiecraane/knowit/flow/BasicFlow.kt
@@ -0,0 +1,7 @@
+package nl.jamiecraane.knowit.flow
+
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+//    flows are cold by default (channels are hot)
+}
