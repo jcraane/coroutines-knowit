7dd094723ca62642876b1492b55e6aa3372509dd
jamiecraane <jcraane@gmail.com>
2020-06-29 17:52:43 +0200

    Add BasicFlow

--- src/main/kotlin/nl/jamiecraane/knowit/flow/BasicFlow.kt
+++ src/main/kotlin/nl/jamiecraane/knowit/flow/BasicFlow.kt
@@ -1,7 +1,28 @@
 package nl.jamiecraane.knowit.flow
 
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking<Unit> {
 //    flows are cold by default (channels are hot)
+//    flows can be declared and only trigger when their collect method is called
+//    thread communication and back pressure is implemented using coroutines
+
+    val stringFlow = flow<String> {
+        for (i in 0..100) {
+            println("Emit value")
+            emit("Emit ${i.toString()}")
+        }
+    }
+
+    // Nothing is in the output if values are not collected
+
+    launch {
+//        collect is a suspending function an must be called from a coroutine
+        stringFlow.collect {
+            println(it)
+        }
+    }
 }
